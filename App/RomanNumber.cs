1aabdb4bfcf1521a261c57c16bf3895a99010519
Bor1sss <78493730+Bor1sss@users.noreply.github.com>
2024-09-12 09:35:12 +0300

    After Refactor

--- App/RomanNumber.cs
+++ App/RomanNumber.cs
@@ -1,107 +1,143 @@
 ﻿using System.Text;
 
-namespace _01_RomanParser;
-
-public record RomanNumber(int Value)
+namespace App
 {
-    private readonly int _value = Value;
-    public int Value { get => _value; init => _value = value; }
-
-    public static RomanNumber Parse(String input)
+    public record RomanNumber(int Value)
     {
-        int value = 0;
-        int prevDigit = 0;
-        int pos = input.Length;
-        List<String> errors = new();
-        foreach (char c in input.Reverse())
-        {
-            pos -= 1;
-            int digit;
-            try
-            {
-                digit = DigitalValue(c.ToString());
-            }
-            catch
-            {
-                errors.Add($"Invalid symbol '{c}' in position {pos}");
-                continue;
-            }
-            if (digit != 0 && prevDigit / digit > 10)
-            {
-                errors.Add($"Invalid order '{c}' before '{input[pos + 1]}' in position {pos}");
-            }
-            
+        private readonly int _value = Value; 
+        public int Value { get => _value; init => _value = value; }
 
-            value += digit >= prevDigit ? digit : -digit;
-            prevDigit = digit;
+
+        public static RomanNumber Parse(String input)
+        {
+            int value = 0;
+            int prevDigit = 0;
+            int pos = input.Length;
+            int maxDigit = 0;
+            bool isLess = false;
+
+            List<String> errors = new();
+
+            foreach (char c in input.Reverse())
+            {
+                pos -= 1;
+                int digit;
+                try
+                {
+                    digit = DigitalValue(c.ToString());
+                }
+                catch
+                {
+                    errors.Add($"Invalid symbol '{c}' in position {pos}");
+                    continue;
+                }
+
+                if (digit != 0 && prevDigit / digit > 10)
+                {
+                    errors.Add($"Invalid order '{c}' before '{input[pos + 1]}' in position {pos}");
+                }
+                
+
+                if (digit < maxDigit)
+                {
+                    if (isLess)
+                    {
+                        throw new FormatException($"Invalid sequence: more than one smaller digit before '{input[pos + 1]}' '{input}'");
+
+                    }
+                    isLess = true;
+                }
+                else
+                {
+                    maxDigit = digit;
+                    isLess = false;
+                }
+
+                value += digit >= prevDigit ? digit : -digit;
+                prevDigit = digit;
+            }
+
+            if (errors.Any())
+            {
+                throw new FormatException(string.Join("; ", errors));
+            }
+
+            return new RomanNumber(value);
         }
 
-        if (errors.Any())
+
+
+
+        public static int DigitalValue(String digit)
         {
-            throw new FormatException(string.Join("; ", errors));
-        }
-
-        return new RomanNumber(value);
-    }
-
-  
-    public static int DigitalValue(String digit)
-    {
-        return digit switch
-        {
-            "N" => 0,
-            "I" => 1,
-            "V" => 5,
-            "X" => 10,
-            "L" => 50,
-            "C" => 100,
-            "D" => 500,
-            "M" => 1000,
-             _  => throw new ArgumentException($" {nameof(RomanNumber)} : {nameof(DigitalValue)}: 'digit' has invalid value '{digit}'")
-        };
-
-    }
-
-    public RomanNumber Plus(RomanNumber other)
-    {
-        return this with { Value = Value + other.Value };
-    }
-    public RomanNumber Plus(string other)
-    {
-        return this with { Value = Value + Parse(other).Value };
-    }
-    public override string? ToString()
-    {
-        if(_value == 0 ) return "N";
-        Dictionary<int, string> parts = new ()
-        {
-            {1000, "M" },
-            {900, "CM" },
-            {500, "D" },
-            {400, "CD" },
-            {100, "C" },
-            {90, "XC" },
-            {50, "L"},
-            {40, "XL"},
-            {10, "X"},
-            {9, "IX"},
-            {5, "V"},
-            {4, "IV"},
-            {1, "I"},
-        };
-
-        int v = _value;
-
-        StringBuilder sb = new ();
-
-        foreach (var part in parts)
-        {
-            while(v >= part.Key)
+            return digit switch
             {
-                v -= part.Key;
-                sb.Append(part.Value);
-            }
+                "N" => 0,
+                "I" => 1,
+                "V" => 5,
+                "X" => 10,
+                "L" => 50,
+                "C" => 100,
+                "D" => 500,
+                "M" => 1000,
+                 _  => throw new ArgumentException($" {nameof(RomanNumber)} : {nameof(DigitalValue)}: 'digit' has invalid value '{digit}'")
+            };
+
         }
-        return sb.ToString();
+
+        public RomanNumber Plus(RomanNumber other)
+        {
+            return this with { Value = Value + other.Value };
+        }
+        public RomanNumber Plus(string other)
+        {
+            return this with { Value = Value + Parse(other).Value };
+        }
+
+
+        public override string? ToString()
+        {
+            //3343->MMMCCCXLIII
+            // MMM
+            // D (500) X
+            // CD (400) X
+            // CCC
+            // LX
+            // III
+            if(_value == 0 ) return "N";
+            Dictionary<int, string> parts = new ()
+            {
+                {1000, "M" },
+                {900, "CM" },
+                {500, "D" },
+                {400, "CD" },
+                {100, "C" },
+                {90, "XC" },
+                {50, "L"},
+                {40, "XL"},
+                {10, "X"},
+                {9, "IX"},
+                {5, "V"},
+                {4, "IV"},
+                {1, "I"},
+            };
+
+
+            int v = _value;
+
+            StringBuilder sb = new ();
+
+            foreach (var part in parts)
+            {
+                while(v >= part.Key)
+                {
+                    v -= part.Key;
+                    sb.Append(part.Value);
+                }
+            }
+            return sb.ToString();
+        }
     }
 }
+
+
